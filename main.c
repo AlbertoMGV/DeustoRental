2fd7a42061330a5cc187fb34226fabc6cba8206e
Iñigo de Mingo <inigo.demingo@opendeusto.es>
2018-04-16 16:59:12 +0200

    mas bonito

--- main.c
+++ main.c
@@ -5,7 +5,9 @@
 
 int main()
 {
-    printf("DeustoRental Company!\n");
+    printf("----------------------------------------------------------\n");
+    printf("                DeustoRental Company!\n");
+    printf("----------------------------------------------------------\n");
 
     Administrador* admin;
     Cliente* cliente;
@@ -13,7 +15,7 @@ int main()
     admin = crearAdministrador(5, "pepe@gmail.com");
     cliente = crearCliente("Ivan", "Sanchez", 19, "C/Falsa, 123, Springfield", "452154852X");
     printf("Admin: id=%i, email=%s\n", admin->id, admin->email);
-    printf("Cliente: dni=%s, nombre y apellidos=%s %s", cliente->dni, cliente->nombre, cliente->apellido);
+    printf("Cliente: dni=%s, nombre y apellidos=%s %s\n", cliente->dni, cliente->nombre, cliente->apellido);
 
     return 0;
 }
